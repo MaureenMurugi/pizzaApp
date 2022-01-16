37651b99a6269c50198ec2e0149be01ee4de57cb
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-01-16 23:39:05 +0300

    add switch case for pizza crust and size

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+function Pizza(pizza, size, crust, toppings) {
+    this.pizza = pizza;
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+}
+
+var priceSize, priceCrust, priceTopping;
+
+var price = function (pizzaSize, pizzaCrust, pizzaTopping) {
+    switch (pizzaSize) {
+        case "":
+            priceSize = 0;
+            break;
+        case "large":
+            priceSize = 1000;
+            break;
+        case "medium":
+            priceSize = 800;
+            break;
+        case "small":
+             priceSize = 550;
+            break;
+        default:
+            location.reload();
+            alert("Please select a pizza size");
+
+    };
+
+    switch (pizzaCrust) {
+        case "":
+            priceCrust = 0;
+            break;
+        case "crispy":
+            priceSize = 100;
+            break;
+        case "stuffed":
+            priceSize = 200;
+            break;
+        case "gluten":
+             priceSize = 150;
+            break;
+        default:
+            location.reload();
+            alert("Please select a pizza crust");
+
+
+}
